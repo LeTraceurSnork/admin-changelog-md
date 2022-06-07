525f95e5f81533efc29236418d6d1538be9f8fa8
Saligzhanov Ilya <saligzhanov.i@spectrumdata.ru>
2022-06-07 10:59:13 +0500

    feat: composer-installers installed

--- changelog.php
+++ changelog.php
@@ -4,17 +4,18 @@
  * @version 1.0.0
  *
  * Plugin Name: Admin CHANGELOG.md
- * Description: Используется когда возникает необходимость показать изменения в последнем CHANGELOG.md внутри админ-панели WP
- * Author: LeTraceurSnork
- * Version: 1.0.0
+ * Description: Используется когда возникает необходимость показать изменения в последнем CHANGELOG.md внутри
+ * админ-панели WP Author: LeTraceurSnork Version: 1.0.0
  */
 
-require_once ABSPATH . "vendor/autoload.php";
+if (is_readable(ABSPATH . "vendor/autoload.php")) {
+    require_once ABSPATH . "vendor/autoload.php";
+}
 
 use SpectrumData\Plugin\Changelog\ChangelogPage;
 
 add_action('admin_menu', function () {
-    add_menu_page('CHANGELOG', 'CHANGELOG', 'manage_options', 'changelog', array(new ChangelogPage(), 'showChangelogPage'));
+    add_menu_page('CHANGELOG', 'CHANGELOG', 'manage_options', 'changelog', [new ChangelogPage, 'showChangelogPage']);
 });
 
 add_action('admin_enqueue_scripts', function () {
